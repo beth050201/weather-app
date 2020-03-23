11680646c18a574a901f14009fbb0496c9b46757
beth050201 <56397955+beth050201@users.noreply.github.com>
2020-03-23 11:12:07 +0000

    fixed

--- app.js
+++ app.js
@@ -17,19 +17,18 @@ const KELVIN = 273;
 const key = "82005d27a116c2880c8f0fcb866998a0";
 
 //geolocation checking first / make sure to turn off ad blocker if installed
-if('geolocation' in navigator) {
+if('geolocation' in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError);
-}
-else{
+}else{
     notificationElement.style.display = "block";
-    notificationElement.innerHTML = "<p> Browser doesn't support Geolocation</p>";
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
 }
 
 // This will set the user's position where ever you are in
-function setPosition(position) {
+function setPosition(position){
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-
+    
     getWeather(latitude, longitude);
 }
 
@@ -39,30 +38,29 @@ function showError(error){
 }
 
 // grab data from provider API / we are bringing in json API data to our app using fetch below
-function getWeather(latitude, longitude) {
+function getWeather(latitude, longitude){
     let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-
-    // below is an object - just the first bit delaing with data
-
+    //below is an object - just the first bit dealing with data
+    
     fetch(api)
-    .then(function(response) {
-        let data = repsonse.json();
-        return data;
-    })
-    .then(function(data){
-        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-        weather.description = data.weather[0].description;
-        weather.iconId = data.weather[0].icon;
-        weather.city = data.name;
-        weather.country = data.sys.country;
-    })
-    .then(function() {
-        displayWeather();
-    });
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
 }
 
 // This function will show the data in the UI with the icons
-function displayWeather() {
+function displayWeather(){
     iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
     tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
     descElement.innerHTML = weather.description;
@@ -74,17 +72,18 @@ function celsiusToFahrenheit(temperature) {
     return (temperature * 9/5) + 32;
 }
 
-// This will trigger the onClick event function to switch from C to F
-tempElement.addEventListener("click", function() {
+/// This will trigger the onClick event function to switch from C to F
+tempElement.addEventListener("click", function(){
     if(weather.temperature.value === undefined) return;
-
-    if (weather.temperature.unit == "celsius") {
+    
+    if(weather.temperature.unit == "celsius"){
         let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
         fahrenheit = Math.floor(fahrenheit);
-
+        
         tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
         weather.temperature.unit = "fahrenheit";
-    }
+    } 
+    
     else {
         tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
         weather.temperature.unit = "celsius"
